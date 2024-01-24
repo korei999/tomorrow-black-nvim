baff9ab32412ec67f2636c597b9e83c23adff093
korei999 <ju7t1xe@gmail.com>
2024-01-24 22:30:06 +0200

    update colors

--- lua/tomorrow-custom/groups/highlights.lua
+++ lua/tomorrow-custom/groups/highlights.lua
@@ -95,7 +95,7 @@ function M.highlight(palette, opts)
         Identifier = { fg = palette.white },
         Function = vim.tbl_extend("force", { fg = palette.white, italic = true }, opts.styles.functions),
         Operator = { fg = palette.light_gray, bold = true },
-        Type = vim.tbl_extend("force", { fg = palette.blue }, opts.styles.type),
+        Type = vim.tbl_extend("force", { fg = palette.blue, bold = true }, opts.styles.type),
         StorageClass = { fg = palette.light_gray, bold = true },
         Structure = { fg = palette.blue, italic = true },
         Structure = vim.tbl_extend("force", { fg = palette.blue, bold = true, italic = true }, opts.styles.structures),
