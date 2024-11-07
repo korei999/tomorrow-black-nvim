9cb155881c53c3576bae84eeb3a5a76ddbfaa0dc
korei999 <ju7t1xe@gmail.com>
2024-11-07 23:20:30 +0200

    status line

--- lua/tomorrow-black/groups/statusline.lua
+++ lua/tomorrow-black/groups/statusline.lua
@@ -2,7 +2,7 @@ local M = {}
 
 function M.highlight(palette, opts)
     return {
-        StatusLine = { fg = palette.gray, bg = palette.bg },
+        StatusLine = { fg = palette.light_gray, bg = palette.alt_bg },
         StatusLineNC = { fg = palette.alt_bg, bg = palette.alt_bg },
         StatusLineSeparator = { fg = palette.bg },
         StatusLineTerm = { fg = palette.bg },
